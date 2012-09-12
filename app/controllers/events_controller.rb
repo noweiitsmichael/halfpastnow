5bdc16db5af46c29da8d5a5cfb5ca6152aa5f616
Rob Rumble <rumblerob@gmail.com>
2012-09-12 18:11:08 -0500

    devise fb fix, splash page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -200,8 +200,6 @@ def index
               LEFT OUTER JOIN events_tags ON events.id = events_tags.event_id
               LEFT OUTER JOIN tags ON tags.id = events_tags.tag_id
             WHERE #{search_match} AND #{occurrence_match} AND #{location_match} AND #{tag_include_match} AND #{tag_exclude_match} AND #{low_price_match} AND #{high_price_match}"
-
-    puts query
     
     @ids = ActiveRecord::Base.connection.select_all(query)
 
