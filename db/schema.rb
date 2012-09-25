7cc709dafff96765e2a9c5597e1d662f63d85b78
khoa le <lmkhoa@gmail.com>
2012-09-25 14:41:09 -0500

    update FB code in config

--- db/schema.rb
+++ db/schema.rb
@@ -91,8 +91,8 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
     t.string   "title"
     t.text     "description"
     t.decimal  "price"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "venue_id"
     t.integer  "clicks",      :default => 0
     t.integer  "views",       :default => 0
@@ -135,8 +135,8 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
     t.datetime "start"
     t.datetime "end"
     t.integer  "event_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "recurrence_id"
     t.integer  "day_of_week"
     t.boolean  "deleted"
@@ -168,8 +168,8 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
     t.text     "url"
     t.string   "raw_id"
     t.string   "from"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "deleted"
     t.boolean  "submitted"
     t.integer  "raw_venue_id"
@@ -192,8 +192,8 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
     t.integer  "review_count"
     t.text     "categories"
     t.text     "neighborhoods"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "raw_id"
     t.string   "from"
     t.integer  "venue_id"
@@ -216,8 +216,8 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
     t.date     "range_end"
     t.datetime "start"
     t.datetime "end"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "event_id"
   end
 
@@ -227,18 +227,27 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
     t.string   "name"
     t.integer  "resource_id"
     t.string   "resource_type"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], :name => "index_roles_on_name"
 
+  create_table "students", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "videoname"
+    t.string   "videonametest"
+  end
+
   create_table "tags", :force => true do |t|
     t.string   "name"
     t.integer  "parent_tag_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "tags", ["parent_tag_id"], :name => "index_tags_on_parent_tag_id"
@@ -246,8 +255,8 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
   create_table "tags_venues", :id => false, :force => true do |t|
     t.integer  "venue_id"
     t.integer  "tag_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
@@ -261,12 +270,13 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
     t.string   "username"
     t.string   "profilepic"
+    t.string   "authentication_token"
     t.string   "provider"
     t.string   "uid"
     t.string   "fb_access_token"
@@ -293,8 +303,8 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
     t.integer  "zip"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "phonenumber"
     t.text     "url"
     t.integer  "clicks",      :default => 0
@@ -303,6 +313,17 @@ ActiveRecord::Schema.define(:version => 20120914204939) do
     t.boolean  "suggested"
     t.text     "fb_picture"
     t.string   "admin_owner"
+  end
+
+  create_table "videos", :force => true do |t|
+    t.integer  "eventID"
+    t.integer  "venueId"
+    t.text     "comment"
+    t.float    "longitude"
+    t.float    "altitude"
+    t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
