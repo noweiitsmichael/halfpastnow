94355be7274cdfa67924fe50e948ec3ddb83f6ab
khoa le <lmkhoa@gmail.com>
2012-11-08 15:33:59 -0600

    Add show artist to mobile controller

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -462,6 +462,33 @@ class MobileController < ApplicationController
     end  
   end
 
+def showact
+  @act = Act.find(params[:id])
+    @occurrences  = []
+    @recurrences = []
+    @pictures = []
+    @occs = @act.events.collect { |event| event.occurrences.select { |occ| occ.start >= DateTime.now }  }.flatten.sort_by { |occ| occ.start }
+    @occs.each do |occ|
+      # check if occurrence is instance of a recurrence
+      if occ.recurrence_id.nil?
+        @occurrences << occ
+      else
+        if @recurrences.index(occ.recurrence).nil?
+          @recurrences << occ.recurrence
+        end
+      end
+    end
+
+    @act.pictures.each do |pic|
+      @pictures << pic
+    end
+
+    respond_to do |format|
+      format.html { render :layout => "mode" }
+      format.json { render json: { :occurrences => @occurrences.to_json(:include => :event), :recurrences => @recurrences.to_json(:include => :event), 
+                                   :act => @act.to_json, :pictures => @pictures.to_json, :embeds => @act.embeds } } 
+    end
+end
   def bookmark
     @userid = User.find_by_email(params[:email]).id
     @occurrenceid = Occurrence.find_by_event_id(params[:event_id]).id
