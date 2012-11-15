4d56780b647cebb3eb06407a3546e47e5ca01b1a
Michael Wei <michael.y.wei@gmail.com>
2012-11-14 23:43:42 -0600

    Starting email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: "support@halfpastnow.com"
+
+  def welcome_email(user)
+    @user = user
+    @url  = "http://halfpastnow.com/login"
+    mail(:to => user.email, :subject => "Welcome to halfpastnow!")
+  end
+end
