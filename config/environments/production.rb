800a2faaafc1250b72ac5860205a4a732716a6f2
Michael Wei <michael.y.wei@gmail.com>
2012-11-15 11:45:52 -0600

    welcome email?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,17 @@ Myapp::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "halfpastnow.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "support@halfpastnow.com",
+    password: "chimeralabs"
+  }
+
+
+
 end
