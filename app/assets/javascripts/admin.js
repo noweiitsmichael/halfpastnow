b25260daf19588b11439069e738eb0d77b1a8045
Michael Wei <michael.y.wei@gmail.com>
2012-12-15 22:20:58 -0600

    fix for date stuff

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui
+//= require jquery-ui-timepicker-addon
+//= require jcrop
+//= require underscore
+//= require jquery-ui
+//= require viewport
+//= require date
+//= require dropkick
+//= require tokeninput
+//= require jquery.validate
+//= require jquery.tabSlideOut.v1.3
+//= require jquery.dataTables.min
+//= require dataTables.fnSetFilteringDelay
+//= require venue
+//= require jquery.cleditor.min
+//= require select2
+//= require jquery.form
+
