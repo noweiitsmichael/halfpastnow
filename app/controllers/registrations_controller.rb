9e65c7aca5d9a610ad7eac249bab5336ab629c8d
khoa le <lmkhoa@gmail.com>
2013-01-15 17:09:55 -0600

    Add FB permission in Profile page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,13 @@ require 'pp'
 class RegistrationsController < Devise::RegistrationsController
   respond_to :html, :xml, :json
   def update
+  	if ( params[:item][:done].to_s == '1') 
+  		puts 'Its checked'
+  	else 
+  		puts 'its not checked'
+  	end
   	self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+  	
 	if params[resource_name][:password].blank? ? resource.update_without_password(params[resource_name]) 
 											   : resource.update_with_password(params[resource_name])			   
 		sign_in resource_name, resource, :bypass => true
