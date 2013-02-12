62714f5b009f58303b9074cdcb6fde59371a68a3
khoa le <lmkhoa@gmail.com>
2013-02-12 01:48:55 -0600

    Mobile controller - Add users for events

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -873,8 +873,8 @@ class MobileController < ApplicationController
     # the big enchilada
     query = "SELECT DISTINCT ON (recurrences.id,users.id) users.id AS user_id, occurrences.end AS end, events.cover_image_url AS cover, venues.phonenumber AS phone, venues.id AS v_id, events.price AS price, events.views AS views, events.clicks AS clicks, acts.id AS act_id, acts.name AS actor, venues.address AS address, venues.state AS state,venues.zip AS zip, venues.city AS city,  recurrences.start AS rec_start, recurrences.end AS rec_end,recurrences.every_other AS every_other,recurrences.day_of_week AS day_of_week,recurrences.week_of_month AS week_of_month,recurrences.day_of_month AS day_of_month ,occurrences.id AS occurrence_id, recurrences.id AS rec_id, events.description AS description, events.title AS title, venues.name AS venue_name, venues.longitude AS longitude, venues.latitude AS latitude, events.id AS event_id, venues.id AS venue_id, occurrences.start AS occurrence_start
             FROM users
-              LEFT OUTER JOIN bookmark_lists ON users.id = bookmark_lists.user_id
-              LEFT OUTER JOIN bookmarks ON bookmark_lists.id =bookmarks.bookmark_list_id 
+              RIGHT OUTER JOIN bookmark_lists ON users.id = bookmark_lists.user_id
+              RIGHT OUTER JOIN bookmarks ON bookmark_lists.id =bookmarks.bookmark_list_id 
               INNER JOIN occurrences ON bookmarks.bookmarked_id = occurrences.id 
               INNER JOIN events ON occurrences.event_id = events.id
               INNER JOIN venues ON events.venue_id = venues.id
@@ -887,8 +887,8 @@ class MobileController < ApplicationController
             UNION
             SELECT DISTINCT ON (events.id,users.id) users.id AS user_id, occurrences.end AS end,events.cover_image_url AS cover,venues.phonenumber AS phone,venues.id AS v_id, events.price AS price, events.views AS views, events.clicks AS clicks, acts.id AS act_id, acts.name AS actor,venues.address AS address, venues.state AS state,venues.zip AS zip, venues.city AS city, occurrences.start AS rec_start, occurrences.end AS rec_end, #{tmp} AS every_other, #{tmp} AS day_of_week, #{tmp} AS week_of_month, #{tmp} AS day_of_month,occurrences.id AS occurrence_id, #{tmp} AS rec_id, events.description AS description, events.title AS title, venues.name AS venue_name, venues.longitude AS longitude, venues.latitude AS latitude, events.id AS event_id, venues.id AS venue_id, occurrences.start AS occurrence_start
             FROM users
-              LEFT OUTER JOIN bookmark_lists ON users.id = bookmark_lists.user_id
-              LEFT OUTER JOIN bookmarks ON bookmark_lists.id =bookmarks.bookmark_list_id 
+              RIGHT OUTER JOIN bookmark_lists ON users.id = bookmark_lists.user_id
+              RIGHT OUTER JOIN bookmarks ON bookmark_lists.id =bookmarks.bookmark_list_id 
               INNER JOIN occurrences ON bookmarks.bookmarked_id = occurrences.id  
               INNER JOIN events ON occurrences.event_id = events.id
               LEFT OUTER JOIN acts_events ON events.id = acts_events.event_id
