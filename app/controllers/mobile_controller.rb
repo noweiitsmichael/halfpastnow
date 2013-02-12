3f894bf0fb3bfe74980a27c2b0ecc3a6cd241fff
khoa le <lmkhoa@gmail.com>
2013-02-12 16:39:07 -0600

    Mobile controller - Add users for events

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -985,7 +985,7 @@ class MobileController < ApplicationController
       users = set.select {|s| s["user_id"].to_i != 0}.collect{|s| s["user_id"].to_i}.uniq
       users = User.find(users).collect{|s| s.uid.to_s}.uniq
       
-      tps = set.collect { |e| { :act_name => e["listid"]}.values }.uniq
+      tps = set.collect { |e|  e["listid"].to_s}.uniq
       
       
       puts tps
