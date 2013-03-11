3c4170e60e56704c1b053beaa475d92c6c0f8781
khoa le <lmkhoa@gmail.com>
2013-03-11 15:38:08 -0500

    Android Friendly - Filter reset bug

--- app/assets/javascripts/half.main.js
+++ app/assets/javascripts/half.main.js
@@ -234,6 +234,13 @@ $(function() {
     var dd = today.getDate();
     var mm = today.getMonth()+1; //January is 0!
     var yyyy = today.getFullYear();
+    if(dd<10){
+      dd='0'+dd
+    }; 
+    if(mm<10){
+      mm='0'+mm
+    }; 
+
     dd = yyyy+"-"+mm+"-"+dd
     var dateString = "start_days=&end_days=";
     if (time==0) { 
