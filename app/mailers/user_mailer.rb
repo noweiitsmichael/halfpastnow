59b00336472e4f62a3a23600c5a3f75c24697379
khoa le <lmkhoa@gmail.com>
2013-03-21 12:57:36 -0500

    Weekly Newsletter

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,6 +11,23 @@ class UserMailer < ActionMailer::Base
   	puts "sending email..."
     @user = user
     @url  = "http://halfpastnow.com/login"
+    event_start = Time.now
+    event_end = event_start.advance(:days => 1)
+    query ="
+    SELECT occurrences.id AS occurrence_id FROM occurrences 
+    INNER JOIN events ON occurrences.event_id = events.id
+    INNER JOIN venues ON events.venue_id = venues.id
+    LEFT OUTER JOIN events_tags ON events.id = events_tags.event_id
+    LEFT OUTER JOIN tags ON tags.id = events_tags.tag_id 
+    WHERE TRUE AND occurrences.start >= '#{event_start}'  AND occurrences.start <= '#{event_end}'"
+    queryResult = ActiveRecord::Base.connection.select_all(query)
+    puts queryResult
+
+    @ids =  queryResult.collect { |e| e["occurrence_id"].to_i }.uniq
+    
+    @ids = @ids[0,5]
+    puts "6 events: "
+    puts @ids
     mail(:to => user.email, :subject => "This week in halfpastnow!")
   end
   
