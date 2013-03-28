44717f958116a0aa59a122e79675f62b08c5010d
khoa le <lmkhoa@gmail.com>
2013-03-28 01:11:49 -0500

    Improve Friend speed

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,11 +7,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 	def facebook
 	    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
+	    puts "First FB"
 	    if @user.persisted?
+	       puts "Second FB"
 	      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
 	      sign_in_and_redirect @user, :event => :authentication
 	    else
+	    	 puts "Third FB"
 	      session["devise.facebook_data"] = request.env["omniauth.auth"]
 	      redirect_to new_user_registration_url
 	    end
