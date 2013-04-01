a3ba1703f0271d88547cdc0b4ffe1bedf79f567b
khoa le <lmkhoa@gmail.com>
2013-04-01 10:51:09 -0500

    Add boolean Subscribe for User

--- db/migrate/20130401154350_remove_subscribe_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveSubscribeFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :subscribe
-      end
-
-  def down
-    add_column :users, :subscribe, :string
-  end
-end
