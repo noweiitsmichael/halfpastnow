a177860a766cfa08622b20e35fd47afe7fd7196e
khoa le <lmkhoa@gmail.com>
2013-04-15 12:44:09 -0500

    Weekly email: change sponsor events-Adding Rogue Running

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -13,4 +13,22 @@ class InfoController < ApplicationController
   def terms
   end
 
+   def unsubscribe
+    
+    e=Email.find_by_email(params[:email])
+    unless e.nil?
+      e.destroy 
+      user =  User.find_by_email(params[:email])
+      unless user.nil?
+        channel = Channel.find(user.ref.to_i)
+        channel.included_tags=""
+        channel.save   
+      end
+     
+     
+    end
+    
+    
+  end
+
 end
