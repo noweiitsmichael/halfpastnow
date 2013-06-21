2ea5f8cf7f9d70b6f05bcf6aeb632c3b881965c3
khoa le <lmkhoa@gmail.com>
2013-06-21 11:47:04 -0500

    Memcachier Update

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1792,7 +1792,9 @@ def FacebookLogin
     
     # For next 5 hours
     if(!params[:next5].to_s.empty?)
-      event_start_date = Time.now
+      # event_start_date = Time.now
+      now = Time.now
+      event_start_date = DateTime.new(now.year, now.month, now.day, now.hour, 0, 0, 0)
     else
       event_start_date = Date.today().advance(:days => (params[:start_days].to_s.empty? ? 0 : params[:start_days].to_i))
     end
