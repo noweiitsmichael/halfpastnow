d94e5e2f148f2611a518bca3af86138b064c56e5
suresh4154 <suresh_torlapati@yahoo.com>
2013-09-03 15:37:14 +0530

    trendsetters page

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Myapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
+    config.cache_store = [:memory_store, {:size => 536870912}]
     
   end
 end
