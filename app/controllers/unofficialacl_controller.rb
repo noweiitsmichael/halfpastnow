695ab5a82de4ef7acdd2d24a1eac38887f335b3d
venky-D <venkat@startupsourcing.com>
2013-09-25 20:07:22 +0530

    Event page changes

--- app/controllers/unofficialacl_controller.rb
+++ app/controllers/unofficialacl_controller.rb
@@ -101,6 +101,22 @@ class UnofficialaclController < ApplicationController
     @event = @occurrence.event
     @venue = @occurrence.event.venue
     @acts = @event.acts
+    @recurrences = []
+
+    @occurrences  = []
+    @recurrences = []
+    @occs = @venue.events.collect { |event| event.occurrences.select { |occ| occ.start >= DateTime.now }  }.flatten.sort_by { |occ| occ.start }
+    @occs.each do |occ|
+      # check if occurrence is instance of a recurrence
+      if occ.recurrence_id.nil?
+        @occurrences << occ
+      else
+        if @recurrences.index(occ.recurrence).nil?
+          @recurrences << occ.recurrence
+        end
+      end
+    end
+
     render layout: "unofficialacl"
   end
 
