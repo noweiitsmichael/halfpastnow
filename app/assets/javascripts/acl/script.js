2c856aff808d4ea24e6a3efc34261759cb3a18e6
Michael Wei <michael.y.wei@gmail.com>
2013-09-25 18:31:18 -0500

    Small changes to ACL site

--- app/assets/javascripts/acl/script.js
+++ app/assets/javascripts/acl/script.js
@@ -77,7 +77,7 @@ $(window).load(function(){
     $(".rotate").textrotator({
       animation: "flip", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
       separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-      speed: 5000 // How many milliseconds until the next word show.
+      speed: 2000 // How many milliseconds until the next word show.
     });
 
 
