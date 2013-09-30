ba59a0889722004043a852a1398b92653713db04
venky-D <venkat@startupsourcing.com>
2013-09-30 18:00:35 +0530

    Date, hover, linkable, tags

--- app/assets/javascripts/acl/vendor/chosen.jquery.js
+++ app/assets/javascripts/acl/vendor/chosen.jquery.js
@@ -107,7 +107,7 @@ Copyright (c) 2011 by Harvest
       this.options = options != null ? options : {};
       this.set_default_values();
       this.is_multiple = this.form_field.multiple;
-      this.default_text_default = this.is_multiple ? "Select Some Options" : "All Tags";
+      this.default_text_default = this.is_multiple ? "Select Some Options" : "Show me everything";
       this.setup();
       this.set_up_html();
       this.register_observers();
