ee4b38a81ae85e2b07866593236484c57d969dce
Michael Wei <michael.y.wei@gmail.com>
2013-10-18 13:45:49 -0500

    suresh and venky's changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130405221528) do
+ActiveRecord::Schema.define(:version => 20131001090606) do
 
   create_table "acts", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
     t.integer  "event_id"
     t.boolean  "suggested"
     t.text     "fb_picture"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.string   "pop_source"
     t.float    "completion"
     t.text     "temp_storage"
+    t.integer  "weight",       :default => 1
   end
 
   add_index "acts", ["event_id"], :name => "index_acts_on_event_id"
@@ -150,8 +151,8 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.string   "title"
     t.text     "description"
     t.decimal  "price"
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "venue_id"
     t.integer  "clicks",          :default => 0
     t.integer  "views",           :default => 0
@@ -164,6 +165,8 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.string   "cover_image_url"
     t.float    "completion"
     t.text     "ticket_url"
+    t.float    "escore",          :default => 0.0
+    t.integer  "weight",          :default => 1
   end
 
   add_index "events", ["clicks"], :name => "index_events_on_clicks"
@@ -208,8 +211,8 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.datetime "start"
     t.datetime "end"
     t.integer  "event_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "recurrence_id"
     t.integer  "day_of_week"
     t.boolean  "deleted"
@@ -218,6 +221,7 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
   add_index "occurrences", ["event_id"], :name => "index_occurrences_on_event_id"
   add_index "occurrences", ["recurrence_id"], :name => "index_occurrences_on_recurrence_id"
   add_index "occurrences", ["start"], :name => "index_occurrences_on_start"
+  add_index "occurrences", ["start"], :name => "occurrences_not_deleted_start_index"
 
   create_table "pictures", :force => true do |t|
     t.string   "image"
@@ -242,8 +246,8 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.text     "url"
     t.string   "raw_id"
     t.string   "from"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "deleted"
     t.boolean  "submitted"
     t.integer  "raw_venue_id"
@@ -270,8 +274,8 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.integer  "review_count"
     t.text     "categories"
     t.text     "neighborhoods"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "raw_id"
     t.string   "from"
     t.integer  "venue_id"
@@ -295,8 +299,8 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.date     "range_end"
     t.datetime "start"
     t.datetime "end"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "event_id"
   end
 
@@ -306,18 +310,26 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.string   "name"
     t.integer  "resource_id"
     t.string   "resource_type"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], :name => "index_roles_on_name"
 
+  create_table "saved_searches", :force => true do |t|
+    t.string   "search_key"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "tags", :force => true do |t|
     t.string   "name"
     t.integer  "parent_tag_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "weight",        :default => 1
   end
 
   add_index "tags", ["parent_tag_id"], :name => "index_tags_on_parent_tag_id"
@@ -325,8 +337,15 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
   create_table "tags_venues", :id => false, :force => true do |t|
     t.integer  "venue_id"
     t.integer  "tag_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "things", :force => true do |t|
+    t.string   "name"
+    t.integer  "parent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
@@ -340,8 +359,8 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
     t.string   "username"
@@ -375,8 +394,8 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.integer  "zip"
     t.float    "latitude"
     t.float    "longitude"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "phonenumber"
     t.text     "url"
     t.integer  "clicks",         :default => 0
@@ -387,6 +406,7 @@ ActiveRecord::Schema.define(:version => 20130405221528) do
     t.integer  "updated_by"
     t.float    "completion"
     t.integer  "assigned_admin"
+    t.integer  "weight",         :default => 1
   end
 
 end
