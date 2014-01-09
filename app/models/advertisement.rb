f84a74397e1befb28e26f3792336ea5f772054c8
Ranjith <ranjith@ranjith-Inspiron-5520.(none)>
2014-01-09 14:05:04 +0530

    changed date format and fixed ads image size

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -9,7 +9,7 @@ class Advertisement < ActiveRecord::Base
   validates :image, :weight, :placement, :start,:end , presence: true
 
   ADV_TYPES = {"Featured Venue" => "featured_venue","Featured Event" => "featured_event", "Featured Artist" =>"featured_artist", "Advertisement - Banner" => "banner_ads", "Advertisement - Details" =>"details_ads"}
-  ADV_TYPES_SHOW = '{"Featured Venue": "featured_venue","Featured Event": "featured_event", "Featured Artist": "featured_artist", "Advertisement - Banner": "banner_ads", "Advertisement - Details": "details_ads"}'
+  ADV_TYPES_SHOW = '{"featured_venue": "Featured Venue", "featured_event": "Featured Event", "featured_artist": "Featured Artist", "banner_ads": "Advertisement - Banner", "details_ads": "Advertisement - Details"}'
   ADV_PLACEMENTS = {
                      :featured => [["Home Page","home_page"],["Search Results","search_results"],["Both", "home_search_pages"]],
                      :banner => [["Search Results","search_results"]],
