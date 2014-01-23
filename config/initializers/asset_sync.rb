2dd5642c64a3697b08f0477a5567a5b095215d77
Prasanna <prasofty@gmail.com>
2014-01-23 11:03:48 +0530

    enabled gzip compression

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -12,7 +12,7 @@ if defined?(AssetSync)
     # config.existing_remote_files = "keep"
     #
     # Automatically replace files with their equivalent gzip compressed version
-    # config.gzip_compression = true
+    config.gzip_compression = true
     #
     # Use the Rails generated 'manifest.yml' file to produce the list of files to
     # upload instead of searching the assets directory.
