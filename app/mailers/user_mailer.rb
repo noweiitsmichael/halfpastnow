fd73edd73c6cd5a52bf5493f69577af96678d710
Michael Wei <michael.y.wei@gmail.com>
2014-03-06 12:04:44 -0600

    uber mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -52,6 +52,22 @@ class UserMailer < ActionMailer::Base
     mail(:to => @user.email, :subject => "Austin's Best of 2013 by the Austin Chronicle", :from => "Half Past Now <support@halfpastnow.com>")
   end
 
+  def uber_email(user)
+    # puts "sending email..."
+    ActionMailer::Base.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "halfpastnow.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "support@halfpastnow.com",
+      password: "chimeralabs"
+    }
+    @user = user
+
+    @url  = "http://www.uber.com/go/hpnatx"
+    mail(:to => @user.email, :subject => "Get free rides during SXSW with Uber!", :from => "Half Past Now <support@halfpastnow.com>")
+  end
 
   def self.find_with(id)
     puts "occurrence.rb"
